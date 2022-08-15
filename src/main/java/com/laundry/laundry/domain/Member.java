cae79859b23493d12eae57445be51dfaf0c8bef0
SeonYeong <tjsdud0280@naver.com>
2022-08-15 18:26:21 +0900

    [기타]어노테이션 공부내용 주석추가

--- src/main/java/com/laundry/laundry/domain/Member.java
+++ src/main/java/com/laundry/laundry/domain/Member.java
@@ -5,17 +5,18 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
+
 @NoArgsConstructor
-@Entity
+@Data // @Getter , @Setter 와 동일함
+@Entity //DB테이블과 같은것 DB상에 테이블명과 class명이 같다면 @Entity(name="member") 뒤에 생략가능
 public class Member {
 
-    @Id
+    @Id //PK일 경우 다는 어노테이션
     @GeneratedValue(strategy = GenerationType.IDENTITY)//PK명시 , id값은 자동으로 +1 하는값
     private int id;
-
+    @Column(name="name") //필드가 컬럼일 경우 추가하는 어노테이션 , DB상의 컬럼 이름과 일치하면 생략가능
     private String name; //이름
+    @Column(name="mobilePhoneNumber")
 
     private String mobilePhoneNumber; //핸드폰 전화
 
